39a073024ef5985c37cad9e285faaf7818c65a58
loreen12 <loreen.anaelle.touron@gmail.com>
2020-07-01 16:56:36 +0200

    Connexion à la base

---
+++ cda-projet-librairie/src/com/cda/connection/MyConnection.java
@@ -0,0 +1,31 @@
+package com.cda.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+public class MyConnection {// nous retourne la connexion à la bdd
+
+	private static Connection connexion = null;
+
+	private MyConnection() {
+		DataSource dataSource = MyDataSourceFactory.getMariaDb();
+
+		try {
+			connexion = dataSource.getConnection();
+			System.out.println("connecté");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnexion() {
+		if (connexion == null) {
+			new MyConnection();
+
+		}
+		return connexion;
+	}
+}
