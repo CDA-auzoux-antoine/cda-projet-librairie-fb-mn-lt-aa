701044b1a5b066dabba6e3adc62159bdce555571
Mnechab <mohamednechab.mn@gmail.com>
2020-07-02 12:54:25 +0200

    conflit StockImp

--- cda-projet-librairie/src/com/cda/dao/StockImp.java
+++ cda-projet-librairie/src/com/cda/dao/StockImp.java
@@ -13,37 +13,6 @@ public class StockImp implements IDao<Stock> {
 	private static Connection c = MyConnection.getConnexion();
 
 	@Override
-<<<<<<< HEAD
-	public void remove(Stock t) {
-
-	}
-
-	@Override
-	public Stock findById(int id) {
-		return null;
-	}
-
-	@Override
-	public List<Stock> getAll() {
-=======
-	public Stock save(Stock t) {
-		return null;
-	}
-
-	@Override
-	public void remove(Stock t) {
-
-	}
-
-	@Override
-	public Stock update(Stock t) {
-
->>>>>>> 554b51a78a7bd5fd82bacb2f54c95b4b5f31be31
-		return null;
-	}
-
-	@Override
-<<<<<<< HEAD
 	public <E> Stock save(E t) {
 		if (c != null && t != null) {
 			try {
@@ -59,19 +28,27 @@ public class StockImp implements IDao<Stock> {
 		} else {
 			System.out.println("Entrée impossible.");
 		}
-=======
-	public Stock findById(int id) {
->>>>>>> 554b51a78a7bd5fd82bacb2f54c95b4b5f31be31
 		return null;
 	}
 
 	@Override
-<<<<<<< HEAD
 	public <E> Stock update(E t) {
 		// TODO Auto-generated method stub
-=======
+		return null;
+	}
+
+	@Override
+	public void remove(Stock t) {
+
+	}
+
+	@Override
+	public Stock findById(int id) {
+		return null;
+	}
+
+	@Override
 	public List<Stock> getAll() {
->>>>>>> 554b51a78a7bd5fd82bacb2f54c95b4b5f31be31
 		return null;
 	}
 
