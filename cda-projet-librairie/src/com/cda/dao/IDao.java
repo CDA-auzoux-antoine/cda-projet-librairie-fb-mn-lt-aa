3849318244f318e7c76741750d2a8071cd922813
Mnechab <mohamednechab.mn@gmail.com>
2020-07-02 13:01:39 +0200

    IDAO

--- cda-projet-librairie/src/com/cda/dao/IDao.java
+++ cda-projet-librairie/src/com/cda/dao/IDao.java
@@ -1,56 +1,17 @@
 package com.cda.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
-import com.cda.connection.MyConnection;
-import com.cda.models.Livre;
-import com.cda.models.Stock;
+public interface IDao<T> {
 
-public class StockImp implements IDao<Stock> {
-	private static Connection c = MyConnection.getConnexion();
+	<E> T save(E t);
 
+	void remove(T t);
 
-	@Override
-	public <E> Stock save(E t) {
-		if (c != null && t != null) {
-			try {
-				String request = "INSERT INTO stock (IdLivre, Quantite) VALUES (?,?)";
-				PreparedStatement ps = null;
-				ps = c.prepareStatement(request);
-				ps.setInt(1, ((Livre) t).getIdentifiant());
-				ps.setInt(2, ((Livre) t).getQuantite());
-				ps.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("EntrÃ©e impossible.");
-		}
-		return null;
-	}
+	<E> T update(E t);
 
-	@Override
-	public <E> Stock update(E t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	T findById(int id);
 
-		@Override
-	public void remove(Stock t) {
-
-	}
-
-	@Override
-	public Stock findById(int id) {
-		return null;
-	}
-
-	@Override
-	public List<Stock> getAll() {
-		return null;
-	}
+	List<T> getAll();
 
 }
