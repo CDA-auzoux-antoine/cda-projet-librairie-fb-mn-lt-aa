33c8f9820b68d1682119d9602ffec94d5b1d036a
antoine <antoine.auzoux@gmail.com>
2020-07-03 16:19:24 +0200

    adresse / clientService modifs

--- cda-projet-librairie/src/com/cda/models/Adresse.java
+++ cda-projet-librairie/src/com/cda/models/Adresse.java
@@ -6,58 +6,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Adresse {
-	private static int idAdresse = 50;
-	private static int numeroRue = 0;
-	private static String nomRue = "rue";
-	private static int codePostal = 0;
-	private static String ville = "ville";
+	private int idAdresse = 50;
+	private int numeroRue = 0;
+	private String nomRue = "rue";
+	private int codePostal = 0;
+	private String ville = "ville";
 
-	public Adresse(int id, int numero, String rue, int codePostal, String ville) {
-		this.idAdresse = id;
+	public Adresse(int numero, String rue, int codePostal, String ville) {
 		this.numeroRue = numero;
 		this.nomRue = rue;
 		this.codePostal = codePostal;
 		this.ville = ville;
-	}
-
-	public static int getIdAdresse() {
-		return idAdresse;
-	}
-
-	public static void setIdAdresse(int idAdresse) {
-		Adresse.idAdresse = idAdresse;
-	}
-
-	public static int getNumeroRue() {
-		return numeroRue;
-	}
-
-	public static void setNumeroRue(int numeroRue) {
-		Adresse.numeroRue = numeroRue;
-	}
-
-	public static String getNomRue() {
-		return nomRue;
-	}
-
-	public static void setNomRue(String nomRue) {
-		Adresse.nomRue = nomRue;
-	}
-
-	public static int getCodePostal() {
-		return codePostal;
-	}
-
-	public static void setCodePostal(int codePostal) {
-		Adresse.codePostal = codePostal;
-	}
-
-	public static String getVille() {
-		return ville;
-	}
-
-	public static void setVille(String ville) {
-		Adresse.ville = ville;
+		getNumeroRue();
 	}
 
 }
