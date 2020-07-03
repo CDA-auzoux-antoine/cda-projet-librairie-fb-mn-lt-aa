544bd600bf04c5a4b187534c3596893759c38a85
CDA-Benseddik-Fethi <58114380+CDA-Benseddik-Fethi@users.noreply.github.com>
2020-07-03 09:30:38 +0200

    Test program

--- cda-projet-librairie/src/com/cda/service/LivreService.java
+++ cda-projet-librairie/src/com/cda/service/LivreService.java
@@ -5,11 +5,21 @@ import com.cda.models.Livre;
 
 public class LivreService {
 
-	public static void modifierPrixLivre(LivreImp pLivreImp, int vPrix, int vId) {
-		Livre livre = pLivreImp.find(vId);
-		livre.setPrix(vPrix);
-		pLivreImp.update(livre);
+	public static void modifierPrixLivre(LivreImp pLivreImp, int vPrix, int pId) {
+		Livre vLivre = pLivreImp.find(pId);
+		vLivre.setPrix(vPrix);
+		pLivreImp.update(vLivre);
 
+	}
+
+	public static boolean modifierQuantiteStockLivre(LivreImp pLivreImp, int pQuantite, int pId) {
+		Livre vLivre = pLivreImp.find(pId);
+		if (vLivre.getIdentifiant() > pQuantite) {
+			vLivre.setQuantite(vLivre.getQuantite() - pQuantite);
+			pLivreImp.update(vLivre);
+			return true;
+		}
+		return false;
 	}
 
 }
